e801dbbb6b7b21c35de0e5c34b00401e3ccda8e6
Amy Hong <amyhong0502@gmail.com>
2018-01-05 15:32:09 -0800

    added index page for testing

---
+++ hotelmanagement/main/src/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hotel Management</title>
+    </head>
+    <body>
+        <main>
+            <p>Hello World</p>
+        </main>
+    </body>
+</html>
