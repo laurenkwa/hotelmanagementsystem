6af86181308c1bd100316d55321a8fe97bae3b0a
Amy Hong <amyhong0502@gmail.com>
2018-01-25 08:10:45 -0800

    added login button and prompt message

--- JavaFX/src/login/Controller.java
+++ JavaFX/src/login/Controller.java
@@ -1,11 +1,18 @@
 package login;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class Controller {
     public Label userId;
     public Label userPassword;
     public TextField userIdTextField;
     public TextField userPasswordTextField;
+    public Text result;
+
+    public void processing(ActionEvent actionEvent) {
+        result.setText("Processing");
+    }
 }
