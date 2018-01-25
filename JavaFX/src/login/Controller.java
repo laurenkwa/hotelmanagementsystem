abe51b61f1492d284d49ae52d49cc501f853cf09
Amy Hong <amyhong0502@gmail.com>
2018-01-24 22:06:14 -0800

    added Basic JavaFX Login GUI

---
+++ JavaFX/src/login/Controller.java
@@ -0,0 +1,11 @@
+package login;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public Label userId;
+    public Label userPassword;
+    public TextField userIdTextField;
+    public TextField userPasswordTextField;
+}
